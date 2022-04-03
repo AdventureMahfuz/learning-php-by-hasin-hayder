114450881815b60bac1153f314630d3730e4c0ba
GinilabDev <80915429+GinilabDev@users.noreply.github.com>
2022-04-04 02:53:25 +0600

    array class 3.1

---
+++ array-class-3.1.php
@@ -0,0 +1,16 @@
+<?php
+$students = array(
+    "Sohel",
+    "Rakib",
+    "Raihan",
+    123,
+    "Shakib"
+);
+var_dump($students);
+echo count($students). "\n\n";
+echo $students[0]. "\n\n";
+
+$arrayCount = count($students);
+for($i = 0; $i < $arrayCount; $i++){
+    echo $students[$i]. "\n";
+}
