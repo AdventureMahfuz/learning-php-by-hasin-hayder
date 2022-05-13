aeb8ae3f27870715ddac294e24f29bfcf4108e8b
GinilabDev <80915429+GinilabDev@users.noreply.github.com>
2022-05-13 15:34:01 +0600

    Create index.php

---
+++ class-4/phpFirstProject/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <title>PHP First Project</title>
+</head>
+<div class="container">
+    <div class="row">
+        <div class="column-60 column-offset-20">
+            <h1>Welcome</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab similique, quas a consectetur soluta odio illum! Ipsum, accusantium. Odio corporis veritatis autem iusto, minus ex magnam? Impedit rerum enim cupiditate assumenda, perferendis facere in temporibus? Deserunt earum, esse mollitia debitis optio voluptatum provident pariatur, doloribus veniam quaerat enim corporis voluptatibus.</p>
+        </div>
+    </div>
+</div>
+<body>
+
+</body>
+
+</html>
